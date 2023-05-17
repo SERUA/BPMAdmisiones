755c711c1702396e1ac34ec92d06254a6e2c1e0d
José García <josegarciar177@gmail.com>
2023-05-16 17:10:12 -0700

    se volvio a comentar lo dle filtro

--- restAPIExtensions/AnahuacBecasRestAPI/src/main/groovy/com/anahuac/rest/api/Index.groovy
+++ restAPIExtensions/AnahuacBecasRestAPI/src/main/groovy/com/anahuac/rest/api/Index.groovy
@@ -44,10 +44,10 @@ class Index implements RestApiController {
             return buildResponse(responseBuilder, HttpServletResponse.SC_BAD_REQUEST,"""{"error" : "the parameter url is missing"}""")
         }
 		
-		SecurityFilter security = new SecurityFilter();
-		if(!security.allowedUrlPost(context,url)){
-			return buildResponse(responseBuilder, HttpServletResponse.SC_FORBIDDEN,"""{"error" : "No tienes permisos"}""")
-		}
+//		SecurityFilter security = new SecurityFilter();
+//		if(!security.allowedUrlPost(context,url)){
+//			return buildResponse(responseBuilder, HttpServletResponse.SC_FORBIDDEN,"""{"error" : "No tienes permisos"}""")
+//		}
 		
 		//VARIABLES===========================================================
 		Integer parameterP = Integer.valueOf(p);
