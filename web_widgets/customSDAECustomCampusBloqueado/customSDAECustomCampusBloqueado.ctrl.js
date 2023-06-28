8606ef5b9d999c5641016f834c47db3efde4fb2c
José García <josegarciar177@gmail.com>
2023-06-28 02:48:11 -0700

    se resolvieron incidencias de becas

--- web_widgets/customSDAECustomCampusBloqueado/customSDAECustomCampusBloqueado.ctrl.js
+++ web_widgets/customSDAECustomCampusBloqueado/customSDAECustomCampusBloqueado.ctrl.js
@@ -25,10 +25,10 @@ function PbButtonCtrl($scope, $http) {
     
     function getCampusBloqueado(){
         let campus = angular.copy($scope.properties.objSolicitudDeAdmision.catCampusEstudio.grupoBonita);
-        if(campus === "CAMPUS-MNORTE" || campus === "CAMPUS-MSUR" || campus === "CAMPUS-MAYAB"){
+        if(campus === "CAMPUS-MNORTE" || campus === "CAMPUS-MSUR" || campus === "CAMPUS-MAYAB" || campus === "CAMPUS-CORDOBA" || campus === "CAMPUS-TAMPICO" || campus === "CAMPUS-XALAPA" || campus === "CAMPUS-PUEBLA"){
             swal({
                 title: "¡Atención!",
-                text: "Para conocer el proceso de solicitud de apoyo educativo, contacta al área de becas del campus.",
+                text: "Para conocer el proceso de solicitud de apoyo educativo, contacta al área de admisiones del campus.",
                 icon: "info"
             }).then(() => {
                 let url = "	/bonita/logoutservice?redirect=false";
