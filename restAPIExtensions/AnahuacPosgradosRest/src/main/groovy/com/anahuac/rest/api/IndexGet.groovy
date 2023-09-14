36de34a093b439986073e0d54f70149037ecac9a
José García <josegarciar177@gmail.com>
2023-09-13 17:27:34 -0700

    se agrego el get al proyecto REST de posgrados, se agrego el servicio del menu v1, aun falta implementar el CRUD para el menu

---
+++ restAPIExtensions/AnahuacPosgradosRest/src/main/groovy/com/anahuac/rest/api/IndexGet.groovy
@@ -0,0 +1,118 @@
+package com.anahuac.rest.api
+
+import groovy.json.JsonBuilder
+
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+import org.apache.http.HttpHeaders
+import org.bonitasoft.web.extension.ResourceProvider
+import org.bonitasoft.web.extension.rest.RestAPIContext
+import org.bonitasoft.web.extension.rest.RestApiController
+import org.bonitasoft.web.extension.rest.RestApiResponse
+import org.bonitasoft.web.extension.rest.RestApiResponseBuilder
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.nio.charset.StandardCharsets
+
+import com.anahuac.rest.api.DAO.UsuariosDAO
+import com.anahuac.rest.api.Entity.Result
+import com.anahuac.rest.api.Security.SecurityFilter
+import org.bonitasoft.engine.identity.UserMembership
+import org.bonitasoft.engine.identity.UserMembershipCriterion
+
+class IndexGet implements RestApiController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(IndexGet.class)
+
+	@Override
+	RestApiResponse doHandle(HttpServletRequest request, RestApiResponseBuilder responseBuilder, RestAPIContext context) {
+		SecurityFilter security = new SecurityFilter();
+		
+		// To retrieve query parameters use the request.getParameter(..) method.
+		// Be careful, parameter values are always returned as String values
+		RestApiResponseBuilder rb;
+		Result result = new Result();
+		def url = request.getParameter "url";
+		
+		//MAPEO DE SERVICIOS==================================================
+		try{
+			switch(url) {
+				case "getMenuAdministrativo":
+					result = new UsuariosDAO().getMenuAdministrativo(context);
+					responseBuilder.withMediaType("application/json")
+					if (result.isSuccess()) {
+						return buildResponse(responseBuilder, HttpServletResponse.SC_OK, new JsonBuilder(result.getData()).toString())
+					}else {
+						return buildResponse(responseBuilder, HttpServletResponse.SC_INTERNAL_SERVER_ERROR,  new JsonBuilder(result).toString())
+					}
+				break;
+			}
+		} catch (Exception e) {
+			e.printStackTrace()
+			result.setSuccess(false)
+			result.setError("fallo por "+e.getMessage())
+		}
+		// Send the result as a JSON representation
+		// You may use buildPagedResponse if your result is multiple
+		if(result.isSuccess()){
+			return buildResponse(responseBuilder, HttpServletResponse.SC_OK, (result.getData().size()>0)?((url.equals("habilitarUsuario"))?result.getData().get(0):result.getData()):new JsonBuilder(result).toString())
+		}else {
+			return buildResponse(responseBuilder, HttpServletResponse.SC_INTERNAL_SERVER_ERROR,  new JsonBuilder(result).toString())
+		}
+	}
+
+	public Result notFound(String url) {
+		Result resultado = new Result();
+		resultado.setSuccess(false);
+		resultado.setError("No se reconoce el servicio: "+url);
+		return resultado
+	}
+	/**
+	 * Build an HTTP response.
+	 *
+	 * @param  responseBuilder the Rest API response builder
+	 * @param  httpStatus the status of the response
+	 * @param  body the response body
+	 * @return a RestAPIResponse
+	 */
+	RestApiResponse buildResponse(RestApiResponseBuilder responseBuilder, int httpStatus, Serializable body) {
+		return responseBuilder.with {
+			withResponseStatus(httpStatus)
+			withResponse(body)
+			build()
+		}
+	}
+
+	/**
+	 * Returns a paged result like Bonita BPM REST APIs.
+	 * Build a response with a content-range.
+	 *
+	 * @param  responseBuilder the Rest API response builder
+	 * @param  body the response body
+	 * @param  p the page index
+	 * @param  c the number of result per page
+	 * @param  total the total number of results
+	 * @return a RestAPIResponse
+	 */
+	RestApiResponse buildPagedResponse(RestApiResponseBuilder responseBuilder, Serializable body, int p, int c, long total) {
+		return responseBuilder.with {
+			withContentRange(p, c, total)
+			withResponse(body)
+			build()
+		}
+	}
+
+	/**
+	 * Load a property file into a java.util.Properties
+	 */
+	Properties loadProperties(String fileName, ResourceProvider resourceProvider) {
+		Properties props = new Properties()
+		resourceProvider.getResourceAsStream(fileName).withStream {
+			InputStream s->
+				props.load s
+		}
+		props
+	}
+
+}
