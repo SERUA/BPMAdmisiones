0c1445165ce24b8d679a15dddd617fd3e98072c6
José García <josegarciar177@gmail.com>
2023-09-20 17:41:25 -0700

    se agrego el script de getCasosArchivadosSDAE

--- restAPIExtensions/AnahuacBecasRestAPI/src/main/groovy/com/anahuac/rest/api/IndexGET.groovy
+++ restAPIExtensions/AnahuacBecasRestAPI/src/main/groovy/com/anahuac/rest/api/IndexGET.groovy
@@ -461,6 +461,16 @@ class IndexGET implements RestApiController {
 						return buildResponse(responseBuilder, HttpServletResponse.SC_INTERNAL_SERVER_ERROR,  new JsonBuilder(result).toString())
 					}
 					break;
+				case "getCasosArchivadosSDAE":
+					String username = request.getParameter "username"
+					result = new CatalogosDAO().getCasosArchivadosSDAE(username);
+					responseBuilder.withMediaType("application/json")
+					if (result.isSuccess()) {
+						return buildResponse(responseBuilder, HttpServletResponse.SC_OK, new JsonBuilder(result.getData()).toString())
+					}else {
+						return buildResponse(responseBuilder, HttpServletResponse.SC_INTERNAL_SERVER_ERROR,  new JsonBuilder(result).toString())
+					}
+				break;
 				default:
 					result = notFound(url);
 					if (result.isSuccess()) {
