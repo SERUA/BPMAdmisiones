758f8ba61fd36335be93df60faa1aeb361b0cb03
José García <josegarciar177@gmail.com>
2023-10-11 12:58:10 -0700

    se realizron ajustes a la solicitud

--- web_widgets/customPSGRwidgetDatosSolicitud/customPSGRwidgetDatosSolicitud.ctrl.js
+++ web_widgets/customPSGRwidgetDatosSolicitud/customPSGRwidgetDatosSolicitud.ctrl.js
@@ -1,13 +1,16 @@
 function ($scope, $http) {
+    
     $scope.$watch("properties.userData", function(){
-       if($scope.properties.userData){
-           let url = "/API/bpm/humanTask?p=0&c=10&f=assigned_id=" + $scope.properties.userData.user_id;
+        if($scope.properties.userData){
+            let url = "../API/bpm/humanTask?p=0&c=10&f=assigned_id=" + $scope.properties.userData.user_id;
            
-           $http.get("url").success(function(data){
-              $scope.properties.taskid = data[0].id;
+            console.log(url);
+            $http.get(url).success(function(data){
+               console.log(data);
+                $scope.properties.taskid = data[0].id;
            }).error(function(){
-               console-log("error al obtener las tareas")
+                console-log("error al obtener las tareas")
            });
-       } 
+        } 
     });
 }
