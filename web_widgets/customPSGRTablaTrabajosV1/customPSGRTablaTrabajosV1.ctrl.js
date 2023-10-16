99c4b16bef41a2cb85b5e9e37da9327003e1401f
José García <josegarciar177@gmail.com>
2023-10-16 16:22:47 -0700

    se hicieron ajustes al formulario de solicitud de posgrados

--- web_widgets/customPSGRTablaTrabajosV1/customPSGRTablaTrabajosV1.ctrl.js
+++ web_widgets/customPSGRTablaTrabajosV1/customPSGRTablaTrabajosV1.ctrl.js
@@ -1,4 +1,4 @@
-function PbTableCtrl($scope) {
+function PbTableCtrl($scope, modalService) {
 
     this.isArray = Array.isArray;
 
@@ -19,4 +19,23 @@ function PbTableCtrl($scope) {
     $scope.removeItem = function(_row){
         $scope.properties.content.splice($scope.properties.content.indexOf(_row), 1);
     }
+    
+    $scope.editar = function(_row, _index){
+        $scope.properties.selectedRow = angular.copy(_row);
+        $scope.properties.selectedRow["editar"] = true;
+        $scope.properties.selectedRow["ver"] = false;
+        $scope.properties.selectedRow["index"] = _index;
+        openModal($scope.properties.modalId);
+    }
+    
+    $scope.ver = function(_row){
+        $scope.properties.selectedRow = angular.copy(_row);
+        $scope.properties.selectedRow["editar"] = false;
+        $scope.properties.selectedRow["ver"] = true;
+        openModal($scope.properties.modalId);
+    }
+    
+    function openModal(modalId) {
+        modalService.open(modalId);
+    }    
 }
---
+++ web_widgets/customPSGRbuttonAgregarTrabajo/customPSGRbuttonAgregarTrabajo.ctrl.js
@@ -0,0 +1,223 @@
+function PbButtonCtrl($scope, $http, $location, $log, $window, localStorageService, modalService) {
+
+    'use strict';
+
+    var vm = this;
+
+    this.action = function action() {
+        if ($scope.properties.action === 'Remove from collection') {
+            removeFromCollection();
+            closeModal($scope.properties.closeOnSuccess);
+        } else if ($scope.properties.action === 'Add to collection') {
+            if(validar()){
+                addToCollection();
+                closeModal($scope.properties.closeOnSuccess);
+            } 
+        } else if ($scope.properties.action === 'Start process') {
+            startProcess();
+        } else if ($scope.properties.action === 'Submit task') {
+            submitTask();
+        } else if ($scope.properties.action === 'Open modal') {
+            closeModal($scope.properties.closeOnSuccess);
+            openModal($scope.properties.modalId);
+        } else if ($scope.properties.action === 'Close modal') {
+            closeModal(true);
+        } else if ($scope.properties.url) {
+            doRequest($scope.properties.action, $scope.properties.url);
+        }
+    };
+
+    function openModal(modalId) {
+        modalService.open(modalId);
+    }
+
+    function closeModal(shouldClose) {
+        if (shouldClose) { modalService.close(); }
+    }
+
+    function removeFromCollection() {
+        if ($scope.properties.collectionToModify) {
+            if (!Array.isArray($scope.properties.collectionToModify)) {
+                throw 'Collection property for widget button should be an array, but was ' + $scope.properties.collectionToModify;
+            }
+            var index = -1;
+            if ($scope.properties.collectionPosition === 'First') {
+                index = 0;
+            } else if ($scope.properties.collectionPosition === 'Last') {
+                index = $scope.properties.collectionToModify.length - 1;
+            } else if ($scope.properties.collectionPosition === 'Item') {
+                index = $scope.properties.collectionToModify.indexOf($scope.properties.removeItem);
+            }
+
+            // Only remove element for valid index
+            if (index !== -1) {
+                $scope.properties.collectionToModify.splice(index, 1);
+            }
+        }
+    }
+
+    function validar(){
+        let valido = true;
+        let mensaje = "";
+
+        if(!$scope.properties.valueToAdd.nombre_empresa){
+            mensaje = "El campo 'Nombre empresa' no debe ir vacío";
+            valido = false;
+        } else if(!$scope.properties.valueToAdd.puesto){
+            mensaje = "El campo 'Puesto' no debe ir vacío";
+            valido = false;
+        } else if(!$scope.properties.valueToAdd.giro_empresa){
+            mensaje = "El campo 'Giro de la empresa' no debe ir vacío";
+            valido = false;
+        } else if(!$scope.properties.valueToAdd.tipo_empresa){
+            mensaje = "El campo 'Tipo de empresa' no debe ir vacío";
+            valido = false;
+        } else if(!$scope.properties.valueToAdd.telefono_empresa){
+            mensaje = "El campo 'Teléfono de la empresa' no debe ir vacío";
+            valido = false;
+        } else if(!$scope.properties.valueToAdd.reporta_a){
+            mensaje = "El campo 'Reporta a' no debe ir vacío";
+            valido = false;
+        } else if(!$scope.properties.valueToAdd.fecha_inicio){
+            mensaje = "El campo 'Fecha de inicio' no debe ir vacío";
+            valido = false;
+        } else if(!$scope.properties.valueToAdd.desc_responsabilidad){
+            mensaje = "El campo 'Descripción de responsabilidades' no debe ir vacío";
+            valido = false;
+        }
+
+        if(!valido){
+            swal("¡Atención!", mensaje, "warning");
+        }
+
+        return valido;
+    }
+
+    function addToCollection() {
+        if (!$scope.properties.collectionToModify) {
+            $scope.properties.collectionToModify = [];
+        }
+        if (!Array.isArray($scope.properties.collectionToModify)) {
+            throw 'Collection property for widget button should be an array, but was ' + $scope.properties.collectionToModify;
+        }
+        var item = angular.copy($scope.properties.valueToAdd);
+
+        if ($scope.properties.collectionPosition === 'First') {
+            $scope.properties.collectionToModify.unshift(item);
+        } else {
+            $scope.properties.collectionToModify.push(item);
+            $scope.properties.valueToAdd = {
+                "grado": "",
+                "programa": "",
+                "institucion": "",
+                "fecha_inicio": "",
+                "fecha_termino": "",
+                "promedio": "",
+                "titulo": false,
+                "pais": null
+            }
+        }
+    }
+
+    function startProcess() {
+        var id = getUrlParam('id');
+        if (id) {
+            var prom = doRequest('POST', '../API/bpm/process/' + id + '/instantiation', getUserParam()).then(function () {
+                localStorageService.delete($window.location.href);
+            });
+
+        } else {
+            $log.log('Impossible to retrieve the process definition id value from the URL');
+        }
+    }
+
+    /**
+     * Execute a get/post request to an URL
+     * It also bind custom data from success|error to a data
+     * @return {void}
+     */
+    function doRequest(method, url, params) {
+        vm.busy = true;
+        var req = {
+            method: method,
+            url: url,
+            data: angular.copy($scope.properties.dataToSend),
+            params: params
+        };
+
+        return $http(req)
+            .success(function (data, status) {
+                $scope.properties.dataFromSuccess = data;
+                $scope.properties.responseStatusCode = status;
+                $scope.properties.dataFromError = undefined;
+                notifyParentFrame({ message: 'success', status: status, dataFromSuccess: data, dataFromError: undefined, responseStatusCode: status });
+                if ($scope.properties.targetUrlOnSuccess && method !== 'GET') {
+                    redirectIfNeeded();
+                }
+                closeModal($scope.properties.closeOnSuccess);
+            })
+            .error(function (data, status) {
+                $scope.properties.dataFromError = data;
+                $scope.properties.responseStatusCode = status;
+                $scope.properties.dataFromSuccess = undefined;
+                notifyParentFrame({ message: 'error', status: status, dataFromError: data, dataFromSuccess: undefined, responseStatusCode: status });
+            })
+            .finally(function () {
+                vm.busy = false;
+            });
+    }
+
+    function redirectIfNeeded() {
+        var iframeId = $window.frameElement ? $window.frameElement.id : null;
+        //Redirect only if we are not in the portal or a living app
+        if (!iframeId || iframeId && iframeId.indexOf('bonitaframe') !== 0) {
+            $window.location.assign($scope.properties.targetUrlOnSuccess);
+        }
+    }
+
+    function notifyParentFrame(additionalProperties) {
+        if ($window.parent !== $window.self) {
+            var dataToSend = angular.extend({}, $scope.properties, additionalProperties);
+            $window.parent.postMessage(JSON.stringify(dataToSend), '*');
+        }
+    }
+
+    function getUserParam() {
+        var userId = getUrlParam('user');
+        if (userId) {
+            return { 'user': userId };
+        }
+        return {};
+    }
+
+    /**
+     * Extract the param value from a URL query
+     * e.g. if param = "id", it extracts the id value in the following cases:
+     *  1. http://localhost/bonita/portal/resource/process/ProcName/1.0/content/?id=8880000
+     *  2. http://localhost/bonita/portal/resource/process/ProcName/1.0/content/?param=value&id=8880000&locale=en
+     *  3. http://localhost/bonita/portal/resource/process/ProcName/1.0/content/?param=value&id=8880000&locale=en#hash=value
+     * @returns {id}
+     */
+    function getUrlParam(param) {
+        var paramValue = $location.absUrl().match('[//?&]' + param + '=([^&#]*)($|[&#])');
+        if (paramValue) {
+            return paramValue[1];
+        }
+        return '';
+    }
+
+    function submitTask() {
+        var id;
+        id = getUrlParam('id');
+        if (id) {
+            var params = getUserParam();
+            params.assign = $scope.properties.assign;
+            doRequest('POST', '../API/bpm/userTask/' + getUrlParam('id') + '/execution', params).then(function () {
+                localStorageService.delete($window.location.href);
+            });
+        } else {
+            $log.log('Impossible to retrieve the task id value from the URL');
+        }
+    }
+
+}
