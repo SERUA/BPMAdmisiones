4438497599f62e0a769787eec58a3aa9e790e3f6
José García <josegarciar177@gmail.com>
2023-10-17 15:00:12 -0700

    se modifico la pantalla de citas nueva para posgrados

--- web_widgets/customPSGRcalendarioCitasV1/customPSGRcalendarioCitasV1.ctrl.js
+++ web_widgets/customPSGRcalendarioCitasV1/customPSGRcalendarioCitasV1.ctrl.js
@@ -4,6 +4,7 @@ function($scope, $http, blockUI, $window) {
         (function(a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
         return check;
     };
+    
     $scope.contadorVerificarTask = 0;
     var vm = this;
     $scope.resultado;
@@ -43,6 +44,7 @@ function($scope, $http, blockUI, $window) {
         $scope.properties.hideCalendario = true;
         $scope.$apply();
     })
+
     $scope.entrevistaSelected = false;
     $scope.sesion_aspirante = {
             "persistenceId": 0,
@@ -106,6 +108,7 @@ function($scope, $http, blockUI, $window) {
                 blockUI.stop();
             });
     }
+
     $scope.$watch('properties.isCorrectTask', function(value) {
         if (angular.isDefined(value) && value !== null) {
             if (!value) {
@@ -206,7 +209,6 @@ function($scope, $http, blockUI, $window) {
         doRequest("GET", "/bonita/API/extension/AnahuacRestGet?url=getPruebasFechas&p=0&c=10&sessionid=" + $scope.sesion.persistenceId + "&aspirante=" + $scope.properties.usuario[0].correoElectronico, null, {}, null, function(datos, extra) {
             $scope.aplicaciones = datos.data
             $scope.modalConfirmar();
-
         })
     }
     $scope.selectFecha = function(ap) {
@@ -223,17 +225,17 @@ function($scope, $http, blockUI, $window) {
             $scope.jsonEntrevista.calle = datos.data[0].calle;
             $scope.jsonEntrevista.numero_int = datos.data[0].numero_int;
             $scope.jsonEntrevista.psicologos = datos.data[0].psicologos;
+
             for (let index = 1; index < datos.data[0].psicologos.length; index++) {
                 const element = datos.data[0].psicologos[index];
                 for (let index2 = 0; index2 < element.lstFechasDisponibles.length; index2++) {
                     const element2 = element.lstFechasDisponibles[index2];
                     $scope.jsonEntrevista.psicologos[0].lstFechasDisponibles.push(element2)
                 }
-
-
             }
         })
     }
+
     $scope.getLstSesion = function() {
         var filtro = {
             "columna": "CAMPUS",
@@ -270,6 +272,7 @@ function($scope, $http, blockUI, $window) {
             "operador": "Igual a",
             "valor": $scope.properties.usuario[0].catPais.persistenceId
         }
+
         $scope.dataToSend.lstFiltro.push(angular.copy(filtro))
 
         filtro = {
@@ -277,12 +280,15 @@ function($scope, $http, blockUI, $window) {
             "operador": "Igual a",
             "valor": ($scope.properties.usuario[0].ciudadExamen == null) ? 0 : $scope.properties.usuario[0].ciudadExamen.persistenceId
         }
+
         $scope.dataToSend.lstFiltro.push(angular.copy(filtro))
+
         filtro = {
             "columna": "PERIODO",
             "operador": "Igual a",
             "valor": $scope.properties.usuario[0].catPeriodo.persistenceId
         }
+
         $scope.dataToSend.lstFiltro.push(angular.copy(filtro))
 
         filtro = {
@@ -290,7 +296,9 @@ function($scope, $http, blockUI, $window) {
             "operador": "Igual a",
             "valor": $scope.properties.usuario[0].correoElectronico
         }
+
         $scope.dataToSend.lstFiltro.push(angular.copy(filtro))
+
         doRequest("POST", "/bonita/API/extension/AnahuacRest?url=getSesionesCalendarioAspirante&p=0&c=10&fecha=" + fechaReporte + "&isMedicina=" + $scope.properties.isMedicina, null, $scope.dataToSend, null, function(datos, extra) {
             scheduler.clearAll();
             scheduler.parse(datos.data, "json");
@@ -323,9 +331,9 @@ function($scope, $http, blockUI, $window) {
         }
     }
 
-
     var selectorFecha = $("div.dhx_cal_date").text();
     var fechaReporte = convertidorFechaCalendario(selectorFecha);
+
     //Fución que se ejecuta cada que se cambia el DOM  en la clase dhx_cal_date
     $('div.dhx_cal_date').bind("DOMSubtreeModified", function() {
         selectorFecha = $("div.dhx_cal_date").text();
@@ -416,6 +424,7 @@ function($scope, $http, blockUI, $window) {
     $scope.setShowCalendar = function() {
         $scope.properties.hideCalendario = false;
     }
+
     $scope.validarSesion = function() {
         var error = false;
         var sweet = { "titulo": "", "texto": "" }
@@ -433,7 +442,9 @@ function($scope, $http, blockUI, $window) {
         }
         return error;
     }
+    
     $scope.datosSelected = {};
+
     $scope.selectEntrevista = function(disponible) {
         if (disponible.disponible && !disponible.ocupado) {
             $scope.entrevistaSelected = true;
@@ -517,13 +528,10 @@ function($scope, $http, blockUI, $window) {
                                     doRequest("POST", `/API/bpm/userTask/${taskId}/execution`, null, {}, null, function(datos, extra) {
 
                                         Swal.fire({
-                                                icon: 'success',
-                                                title: 'Correcto',
-                                                text: `Sesión ${$scope.sesion.nombre+" "} guardada correctamente`,
-                                            })
-                                            ///bonita/portal/resource/app/aspirante/solicitud_iniciada/content/?app=aspirante
-                                            //$window.location.assign("/bonita/portal/resource/app/aspirante/confirmacion_credencial/content/?app=aspirante");
-                                            //window.top.location.href = '/bonita/apps/aspirante/nueva_solicitud/';
+                                            icon: 'success',
+                                            title: 'Correcto',
+                                            text: `Sesión ${$scope.sesion.nombre+" "} guardada correctamente`,
+                                        })
                                         $scope.VerificarTask();
                                     })
                                 });
@@ -538,7 +546,6 @@ function($scope, $http, blockUI, $window) {
                 }
             })
         }
-
     }
 
     $scope.$watch('properties.usuario', function(value) {
@@ -547,7 +554,6 @@ function($scope, $http, blockUI, $window) {
             $scope.loadAsistencias();
         }
     });
-
 
     $scope.loadAsistencias = function() {
         $scope.asistenciaCollegeBoard = false;
@@ -562,17 +568,20 @@ function($scope, $http, blockUI, $window) {
             $scope.loadAsistenciaPsicometrico();
         })
     }
+
     $scope.loadAsistenciaPsicometrico = function() {
         doRequest("GET", "../API/extension/RegistroRest?url=caseVariable&caseid=" + $scope.properties.usuario[0].caseId + "&name=asistenciaPsicometrico", null, null, null, function(datos, extra) {
             $scope.asistenciaPsicometrico = (datos.value === "true");
             $scope.loadAsistenciaEntrevista();
         })
     }
+
     $scope.loadAsistenciaEntrevista = function() {
         doRequest("GET", "../API/extension/RegistroRest?url=caseVariable&caseid=" + $scope.properties.usuario[0].caseId + "&name=asistenciaEntrevista", null, null, null, function(datos, extra) {
             $scope.asistenciaEntrevista = (datos.value === "true");
         })
     }
+
     $scope.VerificarTask = function() {
         doRequest("GET", "/API/extension/RegistroRest?url=humanTask&p=0&c=10&caseid=" + $scope.properties.usuario[0].caseId + "&fstate=ready", null, null, null, function(datos, extra) {
 
