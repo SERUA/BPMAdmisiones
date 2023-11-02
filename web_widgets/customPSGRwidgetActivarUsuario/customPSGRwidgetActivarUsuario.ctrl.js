cdde6ff58bf6b66bf596431d119e7d18e937680a
José García <josegarciar177@gmail.com>
2023-11-01 18:25:42 -0700

    se completo la funcionalidad para e activar usuarios

--- web_widgets/customPSGRwidgetActivarUsuario/customPSGRwidgetActivarUsuario.ctrl.js
+++ web_widgets/customPSGRwidgetActivarUsuario/customPSGRwidgetActivarUsuario.ctrl.js
@@ -1,7 +1,6 @@
 function($scope, $http){
-    
     $scope.$watch("properties.username", function(){
-        if(!$scope.properties.username){
+        if($scope.properties.username){
             habilitarUsuario();
         }
     });
@@ -10,12 +9,13 @@ function($scope, $http){
         let url ="../API/extension/posgradosRestGet?url=habilitarUsuario&usernameAspirante=" + $scope.properties.username;
         
         $http.get(url).success(function(_data){
+            $scope.properties.activarUsuario = _data;
             swal("Ok", "Usuario activado exitosamente.", "success").then(() => {
                 let href = window.location.protocol + "//" + window.location.host +"/apps/login/posgrados/";
                 window.top.location = href;
             });
         }).error(function(_data){
-            debugger;
+            $scope.properties.activarUsuario = _data;
             swal("Algo ha fallado", "El usuario ya ha sido activado anteriormente o no existe.", "error");
         });
     }
