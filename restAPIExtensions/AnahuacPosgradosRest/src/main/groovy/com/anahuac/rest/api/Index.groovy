9dea198b4dbfc18c4bf8c00c99d26b9bfd92bea6
AngelBuitimea <122732894+AngelBuitimea@users.noreply.github.com>
2023-11-28 09:47:59 -0700

    Correción de bandejas  admisiones

--- restAPIExtensions/AnahuacPosgradosRest/src/main/groovy/com/anahuac/rest/api/Index.groovy
+++ restAPIExtensions/AnahuacPosgradosRest/src/main/groovy/com/anahuac/rest/api/Index.groovy
@@ -830,6 +830,38 @@ class Index implements RestApiController {
 						return buildResponse(responseBuilder, HttpServletResponse.SC_INTERNAL_SERVER_ERROR,  new JsonBuilder(result).toString())
 					}
 				break;
+				case "insertCatRequisitosAdicionales":
+					result = new CatalogosDAO().insertCatRequisitosAdicionales(jsonData, context)
+					if (result.isSuccess()) {
+						return buildResponse(responseBuilder, HttpServletResponse.SC_OK, new JsonBuilder(result).toString())
+					}else {
+						return buildResponse(responseBuilder, HttpServletResponse.SC_INTERNAL_SERVER_ERROR,  new JsonBuilder(result).toString())
+					}
+					break;
+				case "deleteCatRequisitosAdicionales":
+					result = new CatalogosDAO().deleteCatRequisitosAdicionales(jsonData, context)
+					if (result.isSuccess()) {
+						return buildResponse(responseBuilder, HttpServletResponse.SC_OK, new JsonBuilder(result).toString())
+					}else {
+						return buildResponse(responseBuilder, HttpServletResponse.SC_INTERNAL_SERVER_ERROR,  new JsonBuilder(result).toString())
+					}
+					break;
+				case "updateCatRequisitosAdicionales":
+					result = new CatalogosDAO().updateCatRequisitosAdicionales(jsonData, context)
+					if (result.isSuccess()) {
+						return buildResponse(responseBuilder, HttpServletResponse.SC_OK, new JsonBuilder(result).toString())
+					}else {
+						return buildResponse(responseBuilder, HttpServletResponse.SC_INTERNAL_SERVER_ERROR,  new JsonBuilder(result).toString())
+					}
+					break;
+				case "getCatRequisitosAdicionales":
+					result = new CatalogosDAO().getCatRequisitosAdicionales(jsonData)
+					if (result.isSuccess()) {
+						return buildResponse(responseBuilder, HttpServletResponse.SC_OK, new JsonBuilder(result.data).toString())
+					}else {
+						return buildResponse(responseBuilder, HttpServletResponse.SC_INTERNAL_SERVER_ERROR,  new JsonBuilder(result).toString())
+					}
+					break;
 				case "insertSesion":
 					result = new SesionesDAO().insertSesion(jsonData)
 					if (result.isSuccess()) {
