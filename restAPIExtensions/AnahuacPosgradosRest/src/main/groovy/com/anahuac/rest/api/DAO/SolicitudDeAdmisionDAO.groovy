d8ad8758c206e0871cb804d0c526f45df71824fb
José García <josegarciar177@gmail.com>
2024-01-05 17:04:32 -0700

    se agregaron campos a la tabla de logtransferencias de PSGR

--- restAPIExtensions/AnahuacPosgradosRest/src/main/groovy/com/anahuac/rest/api/DAO/SolicitudDeAdmisionDAO.groovy
+++ restAPIExtensions/AnahuacPosgradosRest/src/main/groovy/com/anahuac/rest/api/DAO/SolicitudDeAdmisionDAO.groovy
@@ -1283,6 +1283,8 @@ class SolicitudDeAdmisionDAO {
 			
 			closeCon = validarConexion();
 			con.setAutoCommit(false);
+			
+			
 			pstm = con.prepareStatement(Statements.UPDATE_TRANSFERENCIA_REGISTRO);
 			pstm.setLong(1, Long.parseLong(object.campus_transferencia.persistenceId_string));
 			pstm.setLong(2, Long.parseLong(object.caseid));
@@ -1363,16 +1365,28 @@ class SolicitudDeAdmisionDAO {
 				resultado.setData(rows);
 				context.apiClient.processAPI.assignAndExecuteUserTask(context.apiClient.session.userId, tareaEjecutar.id,  contrato);
 				
+				//Bitácora de transferencias
+				Timestamp timestampActual = new Timestamp(System.currentTimeMillis());
+				SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSS");
+				String fechaHoraFormateada = formato.format(timestampActual);
+				
 				pstm = con.prepareStatement(Statements.INSERT_BITACORA_TRANSFERENCIA);
-				pstm.setLong(1, Long.parseLong(object.campus_transferencia.persistenceId_string));
-				pstm.setLong(2, Long.parseLong(object.posgrado_transferencia.persistenceId_string));
-				pstm.setLong(3, Long.parseLong(object.carrera_transferencia.persistenceId_string));
-				pstm.setLong(4, Long.parseLong(object.periodo_transferencia.persistenceId_string));
-				pstm.setLong(5, Long.parseLong(object.caseid));
-	
+				pstm.setLong(1, Long.parseLong(object.caseid));
+				pstm.setLong(2, Long.parseLong(object.campus.persistenceId_string));
+				pstm.setLong(3, Long.parseLong(object.campus_transferencia.persistenceId_string));
+				pstm.setLong(4, Long.parseLong(object.posgrado.persistenceId_string));
+				pstm.setLong(5, Long.parseLong(object.posgrado_transferencia.persistenceId_string));
+				pstm.setLong(6, Long.parseLong(object.carrera.persistenceId_string));
+				pstm.setLong(7, Long.parseLong(object.carrera_transferencia.persistenceId_string));
+				pstm.setLong(8, Long.parseLong(object.periodo.persistenceId_string));
+				pstm.setLong(9, Long.parseLong(object.periodo_transferencia.persistenceId_string));
+				pstm.setString(10, fechaHoraFormateada);
+				pstm.setString(11, context.apiSession.userName);
+				
 				if(pstm.executeUpdate() < 1) {
 					throw new Exception("No se ha podido actualizar el registro, intente de nuevo mas tarde");
 				}
+				//Fin bitácora transferencias
 			}
 			
 			con.commit();
@@ -1395,5 +1409,317 @@ class SolicitudDeAdmisionDAO {
 		return resultado;
 	}
 	
+	public Result selectBitacoraTransferencias(String jsonData, RestAPIContext context) {
+		Result resultado = new Result();
+		Boolean closeCon = false;
+		String where = "",campus = "", orderby = "ORDER BY ", errorlog = ""
+		List < String > lstGrupo = new ArrayList < String > ();
+
+		Long userLogged = 0L;
+		Long caseId = 0L;
+		Long total = 0L;
+		Map < String, String > objGrupoCampus = new HashMap < String, String > ();
+		try {
+			def jsonSlurper = new JsonSlurper();
+			def object = jsonSlurper.parseText(jsonData);
+			def objCatCampusDAO = context.apiClient.getDAO(PSGRCatCampusDAO.class);
+			List < PSGRCatCampus > lstCatCampus = objCatCampusDAO.find(0, 9999);
+			userLogged = context.getApiSession().getUserId();
+			List < UserMembership > lstUserMembership = context.getApiClient().getIdentityAPI().getUserMemberships(userLogged, 0, 99999, UserMembershipCriterion.GROUP_NAME_ASC)
+			for (UserMembership objUserMembership: lstUserMembership) {
+				for (PSGRCatCampus rowGrupo: lstCatCampus) {
+					if (objUserMembership.getGroupName().equals(rowGrupo.getGrupo_bonita())) {
+						lstGrupo.add(rowGrupo.getDescripcion());
+						break;
+					}
+				}
+			}
+
+			assert object instanceof Map;
+			where += " WHERE (regi.is_eliminado = false OR regi.is_eliminado IS null) "
+			
+			if (object.estatusSolicitud != null && object.estatusSolicitud.toString().trim() != "") {
+				where += " AND regi.estatus_solicitud IN ("+object.estatusSolicitud+") "
+			}
+			
+			if (object.caseId != null) {
+				where += " AND regi.caseId = " + object.caseId + " ";
+			}
+			
+			Boolean filtroCampus = false;
+			for (Map < String, Object > filtro: (List < Map < String, Object >> ) object.lstFiltro) {
+				switch (filtro.get("columna")) {
+					case "CAMPUS":
+					filtroCampus = true
+					break;
+				}
+			}
+			
+			if (object.caseId == null && filtroCampus != true) {
+				errorlog += "filtroCampus = " + filtroCampus;
+				if (lstGrupo.size() > 0) {
+					where += " AND ("
+				}
+				
+				for (Integer i = 0; i < lstGrupo.size(); i++) {
+					String campusMiembro = lstGrupo.get(i);
+					where += "camp.descripcion='" + campusMiembro + "'";
+					if (i == (lstGrupo.size() - 1)) {
+						where += ") "
+					} else {
+						where += " OR "
+					}
+				}
+			} else {
+				where += " AND LOWER(camp.DESCRIPCION) = LOWER('" + object.lstFiltro[0].valor + "') ";
+			}
+			
+			if (object.en_requisitos_adicionales_auxiliar != null) {
+				
+				// Buscar las solicitudes en requisitos adicionales usando la tabla auxiliar.
+				if (object.en_requisitos_adicionales_auxiliar === true) {
+					where += " AND regi.caseid IN (" + Statements.SELECT_DISTINCT_CASEID_REQUISITO_ADICIONAL_AUXILIAR + ") ";
+				}
+				else if (object.en_requisitos_adicionales_auxiliar === false) {
+					where += " AND regi.caseid NOT IN (" + Statements.SELECT_DISTINCT_CASEID_REQUISITO_ADICIONAL_AUXILIAR + ") ";
+				}
+			}
+
+			List < Map < String, Object >> rows = new ArrayList < Map < String, Object >> ();
+			closeCon = validarConexion();
+			String SSA = "";
+			pstm = con.prepareStatement(Statements.CONFIGURACIONESSSA)
+			rs = pstm.executeQuery();
+			if (rs.next()) {
+				SSA = rs.getString("valor")
+			}
+			String consulta = Statements.GET_LISTADO_SOLICITUDES;
+			String consultaCount = Statements.GET_CONTEO_SOLICITUDES;
+			if (object.caseId == null) {
+				for (Map < String, Object > filtro: (List < Map < String, Object >> ) object.lstFiltro) {
+					
+					switch (filtro.get("columna")) {
 	
+						case "# EXPEDIENTE":
+							errorlog += "# EXPEDIENTE | "
+							if (where.contains("WHERE")) {
+								where += " AND "
+							} else {
+								where += " WHERE "
+							}
+							where += " LOWER(regi.caseid::VARCHAR) like lower('%[valor]%')";
+							where = where.replace("[valor]", filtro.get("valor"));
+							break;
+						case "NOMBRE,EMAIL,CURP":
+							errorlog += "NOMBRE,EMAIL,CURP"
+							if (where.contains("WHERE")) {
+								where += " AND "
+							} else {
+								where += " WHERE "
+							}
+							where += " ( LOWER(concat(pers.apellido_paterno,' ',pers.apellido_materno,' ',pers.nombre)) like lower('%[valor]%') ";
+							where = where.replace("[valor]", filtro.get("valor"))
+	
+							where += " OR LOWER(regi.correo_electronico) like lower('%[valor]%') ";
+							where = where.replace("[valor]", filtro.get("valor"))
+	
+							where += " OR LOWER(pers.curp) like lower('%[valor]%') ) ";
+							where = where.replace("[valor]", filtro.get("valor"));
+	
+							break;
+						case "PROGRAMA,INGRESO,CAMPUS":
+							errorlog += "PROGRAMA,INGRESO,CAMPUS"
+							if (where.contains("WHERE")) {
+								where += " AND "
+							} else {
+								where += " WHERE "
+							}
+							where += " ( LOWER(posg.descripcion) like lower('%[valor]%') ";
+							where = where.replace("[valor]", filtro.get("valor"))
+	
+							where += " OR LOWER(peri.descripcion) like lower('%[valor]%') ";
+							where = where.replace("[valor]", filtro.get("valor"))
+	
+							where += " OR LOWER(camp.descripcion) like lower('%[valor]%') )";
+							where = where.replace("[valor]", filtro.get("valor"))
+	
+							break;
+						case "PROCEDENCIA,PROMEDIO":
+							errorlog += "PROCEDENCIA,PROMEDIO"
+							if (where.contains("WHERE")) {
+								where += " AND "
+							} else {
+								where += " WHERE "
+							}
+							where += " ( LOWER(posinfo.institucion) like lower('%[valor]%') ";
+							where = where.replace("[valor]", filtro.get("valor"))
+	
+							where += " OR LOWER(posinfo.promedio) like lower('%[valor]%') )";
+							where = where.replace("[valor]", filtro.get("valor"))
+							errorlog += "saliendo de case"
+							break;
+						case "ESTATUS":
+							errorlog += "ESTATUS"
+							if (where.contains("WHERE")) {
+								where += " AND "
+							} else {
+								where += " WHERE "
+							}
+							where += " ( LOWER(estatus.descripcion) like lower('%[valor]%') ) ";
+							where = where.replace("[valor]", filtro.get("valor"))
+							break;
+						case "FECHAREGISTRO":
+							errorlog += "FECHAREGISTRO"
+							if (where.contains("WHERE")) {
+								where += " AND "
+							} else {
+								where += " WHERE "
+							}
+							
+							where += " (LOWER(to_char(regi.fecha_registro::timestamp, 'DD/MM/YYYY HH24:MI')) ";
+							if (filtro.get("operador").equals("Igual a")) {
+								where += "=LOWER('[valor]')"
+							} else {
+								where += "LIKE LOWER('%[valor]%'))"
+							}
+	
+							where = where.replace("[valor]", filtro.get("valor"))
+							break;
+						case "ULTIMA MODIFICACION":
+							errorlog += "ULTIMA MODIFICACION"
+							if (where.contains("WHERE")) {
+								where += " AND "
+							} else {
+								where += " WHERE "
+							}
+							
+							where += " (LOWER(to_char(regi.fecha_ultima_modificacion::timestamp, 'DD/MM/YYYY HH24:MI')) ";
+							if (filtro.get("operador").equals("Igual a")) {
+								where += "=LOWER('[valor]')"
+							} else {
+								where += "LIKE LOWER('%[valor]%'))"
+							}
+	
+							where = where.replace("[valor]", filtro.get("valor"))
+							break;
+						case "MOTIVO":
+							errorlog += "# EXPEDIENTE | "
+							if (where.contains("WHERE")) {
+								where += " AND "
+							} else {
+								where += " WHERE "
+							}
+							where += " LOWER(regi.mensaje_admin_escolar) like lower('%[valor]%')";
+							where = where.replace("[valor]", filtro.get("valor"));
+							break;
+						case "CARRERA":
+							if (where.contains("WHERE")) {
+								where += " AND "
+							} else {
+								where += " WHERE "
+							}
+							where += " LOWER(gest.nombre) like lower('%[valor]%') ";
+							where = where.replace("[valor]", filtro.get("valor"));
+							break;
+						case "PERIODO":
+							if (where.contains("WHERE")) {
+								where += " AND "
+							} else {
+								where += " WHERE "
+							}
+							where += " LOWER(peri.descripcion) like lower('%[valor]%') ";
+							where = where.replace("[valor]", filtro.get("valor"))
+							break;
+						case "ID BANNER":
+							if (where.contains("WHERE")) {
+								where += " AND "
+							} else {
+								where += " WHERE "
+							}
+							where += " LOWER(regi.caseid::VARCHAR) like lower('%[valor]%')";
+							where = where.replace("[valor]", filtro.get("valor"))
+							break;
+						case "NOMBRE DEL ALUMNO":
+							if (where.contains("WHERE")) {
+								where += " AND "
+							} else {
+								where += " WHERE "
+							}
+							where += " LOWER(concat(pers.apellido_paterno,' ',pers.apellido_materno,' ',pers.nombre)) like lower('%[valor]%') ";
+							where = where.replace("[valor]", filtro.get("valor"))
+							break;
+						case "CORREO":
+							if (where.contains("WHERE")) {
+								where += " AND "
+							} else {
+								where += " WHERE "
+							}
+							where += " LOWER(regi.correo_electronico) like lower('%[valor]%') ";
+							where = where.replace("[valor]", filtro.get("valor"))
+							break;
+						default:
+							break;
+					}
+				}
+			}
+			
+			if (object.caseId != null) {
+				orderby = "";
+			} else {
+				switch (object.orderby) {
+					case "ULTIMA MODIFICACION":
+						orderby += "sda.fechaultimamodificacion";
+						break;
+					case "NOMBRE":
+						orderby += "pers.apellido_paterno";
+						break;
+					default:
+						orderby += " regi.persistenceid "
+						break;
+				}
+			}
+			
+			consulta = consulta.replace("[WHERE]", where);
+			consultaCount = consultaCount.replace("[WHERE]", where);
+			
+			pstm = con.prepareStatement(consultaCount);
+			rs = pstm.executeQuery();
+			
+			if (rs.next()) {
+				resultado.setTotalRegistros(rs.getInt("total_registros"));
+			}
+			
+			consulta = consulta.replace("[ORDER_BY]", orderby);
+			consulta = consulta.replace("[LIMIT_OFFSET]", " LIMIT ? OFFSET ?");
+			pstm = con.prepareStatement(consulta);
+			pstm.setInt(1, object.limit);
+			pstm.setInt(2, object.offset);
+			
+			rs = pstm.executeQuery();
+			
+			rows = new ArrayList < Map < String, Object >> ();
+			ResultSetMetaData metaData = rs.getMetaData();
+			int columnCount = metaData.getColumnCount();
+			
+			while (rs.next()) {
+				
+				rows.add(columns);
+			}
+			
+			errorlog = consulta + " 9";
+			resultado.setSuccess(true);
+			resultado.setError_info(errorlog);
+			resultado.setData(rows);
+		} catch (Exception e) {
+			resultado.setError_info(errorlog)
+			resultado.setSuccess(false);
+			resultado.setError(e.getMessage());
+		} finally {
+			if (closeCon) {
+				new DBConnect().closeObj(con, stm, rs, pstm)
+			}
+		}
+		
+		return resultado;
+	}
 }
--- restAPIExtensions/AnahuacPosgradosRest/src/main/groovy/com/anahuac/rest/api/DB/Statements.groovy
+++ restAPIExtensions/AnahuacPosgradosRest/src/main/groovy/com/anahuac/rest/api/DB/Statements.groovy
@@ -64,6 +64,6 @@ class Statements {
 	//Transferencias 
 	public static final String UPDATE_TRANSFERENCIA_REGISTRO = "UPDATE PSGRRegistro SET campus_pid = ? WHERE caseid = ?;";
 	public static final String UPDATE_TRANSFERENCIA_SOLICITUD = "UPDATE PSGRSolAdmiPrograma SET campus_pid = ?, posgrado_pid = ?, programa_interes_pid = ?, periodo_ingreso_pid = ? WHERE caseid = ?;";
-	public static final String INSERT_BITACORA_TRANSFERENCIA = "INSERT INTO PSGRLogTransferencias";
+	public static final String INSERT_BITACORA_TRANSFERENCIA = "INSERT INTO PSGRLogTransferencias (persistenceid, persistenceversion, caseid, campus_origen, campus_destino, lic_origen, lic_destino, periodo_origen, periodo_destino, fecha_transferencia, usuario) VALUES (CASE WHEN (SELECT MAX(persistenceId) + 1 FROM PSGRLogTransferencias) IS NULL THEN 1 ELSE (SELECT max(persistenceId) + 1 FROM PSGRLogTransferencias) END, 0, ? ? ? ? ? ? ? ? ?)";
 	
 }
