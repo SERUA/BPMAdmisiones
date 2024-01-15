cdf43da3f622b8b36618ebb2417bfc9e18a8537b
José García <josegarciar177@gmail.com>
2024-01-15 12:49:48 -0700

    se resolvio un bug que impedia que el selector de campus apareciera cuando solo se tenían dos campus

--- web_widgets/customPSGRcalendarioCitasBackV1/customPSGRcalendarioCitasBackV1.ctrl.js
+++ web_widgets/customPSGRcalendarioCitasBackV1/customPSGRcalendarioCitasBackV1.ctrl.js
@@ -520,4 +520,12 @@ function($scope, $http, blockUI, $window) {
             swal(titulo, mensaje, "error");
         }
     }
+    
+    $scope.limiteFecha = function obtenerFechaActual() {
+      const hoy = new Date();
+      const year = hoy.getFullYear();
+      const month = String(hoy.getMonth() + 1).padStart(2, '0');
+      const day = String(hoy.getDate()).padStart(2, '0');
+      return `${year}-${month}-${day}`;
+    }
 }
