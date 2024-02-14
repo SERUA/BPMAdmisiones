4185da57a0648106700af5a25fb3441b640976d8
Missael Varela <64814475+MissaelVarela@users.noreply.github.com>
2024-02-14 12:51:55 -0700

    fix: bandejaSolicitudesIniciadas funcionando con uni-campus

--- web_widgets/customPSGRtablaSolicitudV1/customPSGRtablaSolicitudV1.ctrl.js
+++ web_widgets/customPSGRtablaSolicitudV1/customPSGRtablaSolicitudV1.ctrl.js
@@ -371,6 +371,14 @@ function PbTableCtrl($scope, $http, $window, blockUI) {
         }
 
         $scope.campusDisponibles = campusDisponibles;
+        
+        // Cuando es campus unico seleccionarlo por defecto.
+        if (campusDisponibles.length === 1) {
+            $scope.selectedCampus = campusDisponibles[0].descripcion;
+            $scope.campusChanged();
+            
+            //$scope.properties.campusSeleccionado = campusDisponibles[0].grupo_bonita;
+        }
     }
 
     // Watchers
