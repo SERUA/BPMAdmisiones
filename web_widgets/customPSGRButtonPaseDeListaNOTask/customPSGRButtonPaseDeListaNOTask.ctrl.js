291216cde8888899dcc1822640980d4263ebbc78
José García <josegarciar177@gmail.com>
2024-03-21 17:34:25 -0700

    se agrego un cambio de estatus al  solicitar modificaciones para que hubspot reconozca el cambio de estatus, se agrego   un llamado al servicio de hubspor para ue actualice cuando pasas lista en la pantalla  de pase de lista

--- web_widgets/customPSGRButtonPaseDeListaNOTask/customPSGRButtonPaseDeListaNOTask.ctrl.js
+++ web_widgets/customPSGRButtonPaseDeListaNOTask/customPSGRButtonPaseDeListaNOTask.ctrl.js
@@ -194,7 +194,45 @@ function PbButtonCtrl($scope, $http, $location, $log, $window, localStorageServi
         return $http(req)
         .success((data)=>{
             debugger;
+            actualizarHubspot();
             console.log("Estatus actualizado");
+            // $scope.properties.dataFromSuccess = data;
+            // $scope.properties.dataFromError = undefined;
+            // $scope.properties.taskCompleted = true;
+
+            // if ($scope.properties.targetUrlOnSuccess && method !== 'GET') {
+            //     redirectIfNeeded();
+            // }
+
+            // closeModal($scope.properties.closeOnSuccess);
+            // if ($scope.properties.caseUserCommentToSend && $scope.properties.caseUserCommentToSend.mensaje !== null) {
+            //     addCaseComment();
+            // }
+
+            // vm.busy = false;
+            // closeModal($scope.properties.closeOnSuccess);
+        })
+        .error(()=>{
+            console.log("Estatus no actualizado");
+        })
+        .finally(function () {
+            localStorageService.delete($window.location.href);
+        });
+    }
+    
+    function actualizarHubspot() {
+        debugger;
+        vm.busy = true;
+        let url = $scope.properties.urlHubspot.replace("[CASEID]", $scope.properties.caseId); 
+
+        var req = {
+            method: 'POST',
+            url: url
+        };
+
+        return $http(req)
+        .success((data)=>{
+            debugger;
             $scope.properties.dataFromSuccess = data;
             $scope.properties.dataFromError = undefined;
             $scope.properties.taskCompleted = true;
