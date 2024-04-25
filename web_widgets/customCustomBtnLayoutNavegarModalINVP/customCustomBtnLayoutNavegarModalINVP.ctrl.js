86348b8f50d57a695270acab001912547e13d00f
José García <josegarciar177@gmail.com>
2024-04-24 17:00:17 -0700

    se resolvieron incidendias del INVP

--- web_widgets/customCustomBtnLayoutNavegarModalINVP/customCustomBtnLayoutNavegarModalINVP.ctrl.js
+++ web_widgets/customCustomBtnLayoutNavegarModalINVP/customCustomBtnLayoutNavegarModalINVP.ctrl.js
@@ -1,9 +1,10 @@
 function PbButtonCtrl($scope, modalService) {
     $scope.changeVariable = function(){
+        let idioma = angular.copy($scope.properties.idioma);
         
         swal({
-            title: "Confirmación", 
-            text: "Una vez que envies el examen, no podrás cambiar o concluir tus respuestas",
+            title: idioma === "ESP" ? "Confirmación" : "Confirmation", 
+            text: idioma === "ESP" ? "Una vez que envies el examen, no podrás cambiar o concluir tus respuestas" : "Once you send your exam, you won't be able to change or finish your answers",
             icon: "warning",
             // buttons: [
             //     'Cancelar',
@@ -11,12 +12,12 @@ function PbButtonCtrl($scope, modalService) {
             // ]
             buttons: {
                 no: {
-                    text: "Cancelar",
+                    text: idioma === "ESP" ? "Cancelar" : "Cancel",
                     value: false,
                     className:'btn-info'
                 },
                 si: {
-                    text: "Enviar todo y terminar",
+                    text: idioma === "ESP" ? "Enviar todo y terminar" : "Send and finish",
                     value: true,
                     className:'btn-primary'
                 }
